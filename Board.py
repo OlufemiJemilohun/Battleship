a7a6819d3e6926cb5c029ab3f0d0651903a90667
Femi Jemilohun <fjemilo7@gmail.com>
2016-11-17 15:29:24 -0500

    Update Board.py
    Used the .join function to remove the qoutes and commas when printing the board

--- Board.py
+++ Board.py
@@ -3,8 +3,8 @@ board = []
 for loop in range (0,4):
 	board.append(["O"] * 5)
 
-#function to print the board
+#simple function to print the board
 def print_board(board):
 	for i in board:
-		print board[i]
+		print " ".join(board[i]) """us the join function to combine items in string"""
 
