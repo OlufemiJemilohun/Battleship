378efebf97fac4ce4501acbaba8c65963d0a7c98
Femi Jemilohun <fjemilo7@gmail.com>
2016-11-17 15:25:43 -0500

    Update Board.py

--- Board.py
+++ Board.py
@@ -1,6 +1,10 @@
 #create a 5x5 board of "O"
 board = []
 for loop in range (0,4):
-		board.append(["O"] * 5)
-#print the board
-print Board
+	board.append(["O"] * 5)
+
+#function to print the board
+def print_board(board):
+	for i in board:
+		print board[i]
+
