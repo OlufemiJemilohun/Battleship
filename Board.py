d70facb38278dda8709a7093142a775cc938d446
Femi Jemilohun <fjemilo7@gmail.com>
2016-11-18 05:31:53 -0500

    Update Board.py
    Hide our battleship in a random location on the board using the randint function from the random module.

--- Board.py
+++ Board.py
@@ -1,3 +1,8 @@
+#
+
+#import randint function from the the random module
+from random import randint
+
 #create a 5x5 board of "O"
 board = []
 for loop in range (0,4):
@@ -8,3 +13,8 @@ def print_board(board):
 	for i in board:
 		print " ".join(board[i]) """us the join function to combine items in string"""
 
+#hide our battleship in a random location on the board.
+def random_row(board):
+	return randint(0, len(board) - 1)
+def random_col(board):
+	return randint(0, len(board) - 1)
