abe6ab732a07c31571f36b6530c355a9db07b83c
Femi Jemilohun <fjemilo7@gmail.com>
2016-11-22 08:29:43 -0500

    Update Board.py
    Comment out debugs

--- Board.py
+++ Board.py
@@ -24,7 +24,7 @@ ship_row = random_row(board)
 ship_col = random_col(board)
 
 #debugging - print location of the ship. Comment out later
-print "Row: " + str(ship_row) + ", " + "Column: " + str(ship_col)
+"""print "Row: " + str(ship_row) + ", " + "Column: " + str(ship_col)"""
 
 #use the raw_input function to allow player to guess the row/column of the battleship
 guess_row = int(raw_input("Guess Row: "))
@@ -34,8 +34,9 @@ guess_col = int(raw_input("Guess Column: "))
 if guess_row == ship_row and guess_col == ship_col:
 	print "Congratulations! You sank my battleship!"
 #player enters location that is not on the board
-elif (guess_row, guess_col) not in range(len(board) - 1):    
-	print len(board)    
+elif (guess_row, guess_col) not in range(len(board)):    
+	#debug
+	"""print len(board)"""    
 	print "Oops, that's not even in the ocean."
 #player enters location that has already been guessed
 elif board[guess_col][guess_row] == "X":    
