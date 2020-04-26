106c3beaca82f8752ef673c9b0f3931db2144fdb
Femi Jemilohun <olufemi.jemilohun@gmail.com>
2020-04-25 21:48:53 -0400

    Created 8x8 board for a single player

---
+++ game/board.py
@@ -0,0 +1,27 @@
+'''This script is used to create the boards for the game
+'''
+
+# Linear Algebra
+import numpy as np
+# Data modeling
+from dataclasses import dataclass, field, asdict
+import json
+# Type hints
+from typing import List, Dict
+
+@dataclass
+class Board:
+  '''Create the 8x8 boards for one player
+  '''
+  current: np.array = None
+  previous: np.array = None
+  
+  def __post_init__(self):
+    '''Create current/previous board to track moves
+    '''
+    self.current = np.zeros(shape=(8,8))
+    self.previous = np.zeros(shape=(8,8))
+  
+if __name__ == '__main__':
+  B = Board()
+  print(asdict(B))
